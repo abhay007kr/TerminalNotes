f9c5752e4c4abe541db311ad03aec7878351e581
abhay007kr <aksabhay007@gmail.com>
2020-07-08 19:35:29 +0530

    chalk to provide color to console log

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 const { loadNote } = require('../Notes/notes')
 
 //Add a note
@@ -58,6 +59,13 @@ const removeNote = function(title){
         const notesToKeep = notes.filter(function(note){
             return note.title !== title
         })
+
+        if(notes.length>notesToKeep.length){
+            console.log(chalk.green.inverse('Note Removed'))
+            saveNotes(notesToKeep)
+        }else{
+            console.log(chalk.red.inverse('No note found'))
+        }
         saveNotes(notesToKeep)
 }
 
