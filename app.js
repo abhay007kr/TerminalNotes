81b13cfb812939ce0433057f58eaa444cd87bcf2
abhay007kr <aksabhay007@gmail.com>
2020-07-08 19:03:11 +0530

    remove command function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const yargs = require('yargs')
 const notes = require('./notes')
 const { describe, demandOption ,argv} = require('yargs')
 
+//Command For Adding Notes
 yargs.command({
     command:'add',
     describe:'Add Notes',
@@ -19,6 +20,24 @@ yargs.command({
     },
     handler:function(){
         notes.addNote(argv.title,argv.body)
+    }
+})
+
+
+//Command for removing Notes
+
+yargs.command({
+    command:'remove',
+    describe:'Remove an existing notes',
+    builder:{
+        title:{
+            demandOption:true,
+            type:'string',
+            describe:'notes title'
+        }
+    },
+    handler:function(){
+        console.log(argv.title)
     }
 })
 
