dc5cf3bbff06699b2c3ca839a1ba54713aff6499
abhay007kr <aksabhay007@gmail.com>
2020-07-08 17:31:12 +0530

    add function added

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const yargs = require('yargs')
 const notes = require('./notes')
-const { describe } = require('yargs')
+const { describe, demandOption ,argv} = require('yargs')
+// const { argv } = require('process')
 
 yargs.command({
     command:'add',
     describe:'Add Notes',
     builder:{
-        
+        title:{
+            describe:'Notes Title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Notes Body',
+            demandOption:true,
+            type:'string'
+        }
     },
     handler:function(){
-
+        console.log("Title :" + argv.title)
+        console.log("Body :" + argv.body)
     }
 })
+
+yargs.parse()
