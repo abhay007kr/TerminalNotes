7d9aa4dfaa67d00d3659c96ad087d79e44b87792
abhay007kr <aksabhay007@gmail.com>
2020-07-08 17:54:45 +0530

    loading Json file

--- notes.js
+++ notes.js
@@ -4,9 +4,12 @@ const { loadNote } = require('../Notes/notes')
 //Add a note
 const addNote = function(title,body){
     const notes = loadNotes() //First load a note
+    
 }
 
 //To check if the file named "notes.json" exists 
 function loadNote(){
-    // const dataBuffer = fs.readFileSync('notes.json') // To check i
+    const dataBuffer = fs.readFileSync('notes.json') 
+    const dataJson = JSON.stringify(dataBuffer) //Convert Buffered data into string
+    return JSON.parse(dataJson)
 }
