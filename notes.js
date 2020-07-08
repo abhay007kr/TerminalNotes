eba5ae03f5b548b9bd474dcaf7cd3c221e4bdb1b
abhay007kr <aksabhay007@gmail.com>
2020-07-08 17:57:30 +0530

    Fixed errors for loading notes

--- notes.js
+++ notes.js
@@ -4,12 +4,17 @@ const { loadNote } = require('../Notes/notes')
 //Add a note
 const addNote = function(title,body){
     const notes = loadNotes() //First load a note
-    
+    console.log(notes)
 }
 
 //To check if the file named "notes.json" exists 
 function loadNote(){
-    const dataBuffer = fs.readFileSync('notes.json') 
+    try{
+        const dataBuffer = fs.readFileSync('notes.json') 
     const dataJson = JSON.stringify(dataBuffer) //Convert Buffered data into string
     return JSON.parse(dataJson)
+    }catch(e){
+        return []
+    }
+    
 }
