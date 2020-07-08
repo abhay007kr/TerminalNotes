01bf50882d305ac15ff0b73e4e1093195448540b
abhay007kr <aksabhay007@gmail.com>
2020-07-08 17:48:53 +0530

    added addNote and LoadNote empty function

---
+++ notes.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const { loadNote } = require('../Notes/notes')
+
+//Add a note
+const addNote = function(title,body){
+    const notes = loadNotes() //First load a note
+}
+
+//To check if the file named "notes.json" exists 
+function loadNote(){
+    // const dataBuffer = fs.readFileSync('notes.json') // To check i
+}
