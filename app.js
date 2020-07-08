cdc247818a67b457657b7359a828460cb47f9b12
abhay007kr <aksabhay007@gmail.com>
2020-07-08 17:16:09 +0530

    empty command for add notes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('working')
+const yargs = require('yargs')
+const notes = require('./notes')
+const { describe } = require('yargs')
+
+yargs.command({
+    command:'add',
+    describe:'Add Notes',
+    builder:{
+        
+    },
+    handler:function(){
+
+    }
+})
